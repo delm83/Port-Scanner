08edd2d2c0909bf06fa7d3afd6e8564600464cf0
delm83 <delm83@googlemail.com>
2024-08-30 14:22:56 +0100

    can pass first 4 tests

--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,21 @@
 import socket
+from common_ports import ports_and_services
 
 def get_open_ports(target, port_range, verbose = False):
-    open_ports = []
-
-
-
+    if not verbose:
+        open_ports = []
+        for port in range(port_range[0], port_range[1]+1): 
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.5) 
+            if not s.connect_ex((target, port)):
+                open_ports.append(port)
+            s.close()
+    else: 
+        open_ports = f'Open ports for {target}\nPORT     SERVICE\n'
+        for port in range(port_range[0], port_range[1]+1): 
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.4) 
+            if not s.connect_ex((target, port)):
+                open_ports += (f"{port}{' '*(9-len(str(port)))}{ports_and_services[port]}\n").rstrip()
+            s.close()
     return(open_ports)
