870e753cfa4fda3e2801e6014d24ac9380c528c0
delm83 <delm83@googlemail.com>
2024-08-30 19:46:53 +0100

    can pass first 6 tests

--- port_scanner.py
+++ port_scanner.py
@@ -6,16 +6,23 @@ def get_open_ports(target, port_range, verbose = False):
         open_ports = []
         for port in range(port_range[0], port_range[1]+1): 
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(0.5) 
+            s.settimeout(0.6) 
             if not s.connect_ex((target, port)):
                 open_ports.append(port)
             s.close()
     else: 
-        open_ports = f'Open ports for {target}\nPORT     SERVICE\n'
+        if target.replace('.', '').isnumeric():
+            try:
+                hostname = socket.gethostbyaddr(target)[0]
+            except:
+                hostname = False
+            open_ports = f'Open ports for {hostname} ({(target)})\nPORT     SERVICE' if hostname else f'Open ports for {target}\nPORT     SERVICE'
+        else: 
+            open_ports = f'Open ports for {target} ({socket.gethostbyname(target)})\nPORT     SERVICE'
         for port in range(port_range[0], port_range[1]+1): 
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(0.4) 
+            s.settimeout(0.6) 
             if not s.connect_ex((target, port)):
-                open_ports += (f"{port}{' '*(9-len(str(port)))}{ports_and_services[port]}\n").rstrip()
+                open_ports += (f"\n{port}{' '*(9-len(str(port)))}{ports_and_services[port]}").rstrip()
             s.close()
     return(open_ports)
